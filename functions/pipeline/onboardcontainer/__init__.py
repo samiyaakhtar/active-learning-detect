b4bdf4be2fa40bc8ea135a7e92149f99358a2fa1
Bhargav Nookala <nooknb@gmail.com>
2018-12-05 19:13:35 -0800

    Using signed URLs in onboarding container (#65)

--- functions/pipeline/onboardcontainer/__init__.py
+++ functions/pipeline/onboardcontainer/__init__.py
@@ -2,10 +2,14 @@ import os
 import logging
 import json
 import azure.functions as func
+from datetime import datetime, timedelta
 
-from azure.storage.blob import BlockBlobService
+from azure.storage.blob import BlockBlobService, BlobPermissions
 from azure.storage.queue import QueueService, QueueMessageFormat
-DEFAULT_RETURN_HEADER= { "content-type": "application/json"}
+
+DEFAULT_RETURN_HEADER = {
+    "content-type": "application/json"
+}
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -27,33 +31,59 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         storage_account_key = req_body["storageAccountKey"]
         storage_container = req_body["storageContainer"]
     except ValueError:
-        return func.HttpResponse("ERROR: Unable to decode POST body", status_code=400)
+        return func.HttpResponse(
+            "ERROR: Unable to decode POST body",
+            status_code=400
+        )
 
     if not storage_container or not storage_account or not storage_account_key:
-        return func.HttpResponse("ERROR: storage container/account/key/queue not specified.", status_code=401)
+        return func.HttpResponse(
+            "ERROR: storage container/account/key/queue not specified.",
+            status_code=401
+        )
 
     # Create blob service for storage account (retrieval source)
     blob_service = BlockBlobService(account_name=storage_account,
                                     account_key=storage_account_key)
 
     # Queue service for perm storage and queue
-    queue_service = QueueService(account_name=os.getenv('STORAGE_ACCOUNT_NAME'),
-                                 account_key=os.getenv('STORAGE_ACCOUNT_KEY'))
+    queue_service = QueueService(
+        account_name=os.getenv('STORAGE_ACCOUNT_NAME'),
+        account_key=os.getenv('STORAGE_ACCOUNT_KEY')
+    )
+
     queue_service.encode_function = QueueMessageFormat.text_base64encode
 
     try:
         blob_list = []
-        for blob_object in blob_service.list_blobs(storage_container):
-            blob_list.append(blob_service.make_blob_url(storage_container, blob_object.name))
 
-        # TODO: Batch messaging?
-        for image_url in blob_list:
+        for blob_object in blob_service.list_blobs(storage_container):
+            logging.info("INFO: Building sas token for blob " + blob_object.name)
+            # create sas signature
+            sas_signature = blob_service.generate_blob_shared_access_signature(
+                storage_container,
+                blob_object.name,
+                BlobPermissions.READ,
+                datetime.utcnow() + timedelta(hours=1)
+            )
+
+            blob_url = blob_service.make_blob_url(
+                storage_container,
+                blob_object.name
+            )
+
+            signed_url = blob_url + "?" + sas_signature
+
+            blob_list.append(signed_url)
+
+            logging.info("INFO: Built signed url " + signed_url)
+
             msg_body = {
-                "imageUrl": image_url,
+                "imageUrl": signed_url,
                 "userName": user_name
             }
-            body_str = json.dumps(msg_body)
 
+            body_str = json.dumps(msg_body)
             queue_service.put_message("onboardqueue", body_str)
 
         return func.HttpResponse(
