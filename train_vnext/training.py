cb18c8b4ba03dad4faa85e0df76417d0b1a77206
Samiya Akhtar <saakhta@microsoft.com>
2018-12-17 15:48:36 -0800

    Uploading prediction labels and performance to db

--- train_vnext/training.py
+++ train_vnext/training.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import csv
 import requests
+import datetime
 from azure.storage.blob import BlockBlobService, ContentSettings
 from utils.blob_utils import BlobStorage
 from utils.config import Config
@@ -10,7 +11,8 @@ import urllib.request
 import sys
 import time
 import jsonpickle
-from functions.pipeline.shared.db_access import ImageTagState, PredictionLabel
+import json
+from functions.pipeline.shared.db_access import ImageTagState, PredictionLabel, TrainingSession
 
 CONFIG_PATH = os.environ.get('ALCONFIG', None)
 
@@ -119,26 +121,7 @@ def convert_tagged_labels_to_csv(data, tagged_output_file_path):
         print("An error occurred attempting to write to file at {0}:\n\n{1}".format(tagged_output_file_path,e))
         raise
 
-def upload_data_post_training(prediction_output_file, training_id, user_name,function_url):
-    function_url = function_url + "/api/classification"
-    query = {
-        "userName": user_name
-    }
-
-    # First, we need to get a mapping of class names to class ids
-    classes = ""
-    with open(prediction_output_file) as f:
-        content = f.readlines()
-        for line in content:
-            class_name = line.strip().split(',')[1]
-            if class_name not in classes:
-                classes = classes + class_name + ","
-    
-    query["className"] = classes[:-1]
-    response = requests.get(function_url, params=query)
-    classification_name_to_class_id = response.json()
-    
-    # Now that we have a mapping, we create prediction labels in db
+def upload_data_post_training(prediction_output_file, classification_name_to_class_id, training_id, user_name,function_url):    
     query = {
         "userName": user_name,
         "trainingId": training_id
@@ -146,6 +129,7 @@ def upload_data_post_training(prediction_output_file, training_id, user_name,fun
     function_url = function_url + "/api/labels"
     payload_json = process_post_training_csv(prediction_output_file, training_id, classification_name_to_class_id)
     requests.post(function_url, params=query, json=payload_json)
+    print("Uploaded prediction labels to db.")
 
 def process_post_training_csv(csv_path, training_id, classification_name_to_class_id):
     payload_json = []
@@ -158,16 +142,84 @@ def process_post_training_csv(csv_path, training_id, classification_name_to_clas
                 prediction_label = PredictionLabel(training_id, 
                                     int(row[0].split('.')[0]), 
                                     classification_name_to_class_id[class_name], 
-                                    float(row[2]), 
-                                    float(row[3]), 
-                                    float(row[4]), 
-                                    float(row[5]), 
+                                    float(row[2])*float(row[6]), 
+                                    float(row[3])*float(row[6]), 
+                                    float(row[4])*float(row[7]), 
+                                    float(row[5])*float(row[7]), 
                                     int(row[6]), 
                                     int(row[7]), 
                                     float(row[8]), 
                                     float(row[9]))
                 payload_json.append(prediction_label)
     return jsonpickle.encode(payload_json, unpicklable=False)
+
+def save_training_session(config, model_location, perf_location, prediction_labels_location):
+    cur_date_time = "{date:%Y_%m_%d_%H_%M_%S}".format(date=datetime.datetime.now())
+    file_name = "frozen_inference_graph_" + cur_date_time + ".pb"
+    # First, upload the model to blob storage
+    model_location = upload_model_to_blob_storage(config, model_location, file_name)
+
+    # Get the mapping for class name to class id
+    overall_average, classification_name_to_class_id = get_average_and_classification_map(perf_location, config.get("tagging_user"), config.get("url"))
+
+    # Create a new training session in db and get its id
+    training_id = construct_new_training_session(perf_location, classification_name_to_class_id, overall_average, cur_date_time, model_location, config.get("tagging_user"), config.get("url"))
+
+    # Upload prediction labels to the db
+    upload_data_post_training(prediction_labels_location, classification_name_to_class_id, training_id, config.get("tagging_user"), config.get("url"))
+
+def upload_model_to_blob_storage(config, model_location, file_name):
+    blob_storage = BlobStorage.get_azure_storage_client(config)
+    uri = 'https://' + config.get("storage_account") + '.blob.core.windows.net/' + config.get("storage_container") + '/' + file_name
+    blob_storage.create_blob_from_path(
+            config.get("storage_container"),
+            file_name,
+            model_location
+        )
+    print("Model uploaded at " + str(uri))
+    return uri
+
+def construct_new_training_session(perf_location, classification_name_to_class_id, overall_average, training_description, model_location, user_name, function_url):
+    avg_dictionary = {}
+    with open(perf_location) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader, None) #Skip header
+        for row in reader:
+            if row[0] != "NULL" and row[0] in classification_name_to_class_id:
+                avg_dictionary[classification_name_to_class_id[row[0]]] = row[1]
+    training_session = TrainingSession(training_description, model_location, overall_average, avg_dictionary)
+    query = {
+        "userName": user_name
+    }
+    function_url = function_url + "/api/train"
+    payload = jsonpickle.encode(training_session, unpicklable=False)
+    response = requests.post(function_url, params=query, json=payload)
+    training_id = int(response.json())
+    print("Created a new training session with id: " + str(training_id))
+    return training_id
+
+def get_average_and_classification_map(csv_file, user_name,function_url):
+    classes = ""
+    query = {
+        "userName": user_name
+    }
+    function_url = function_url + "/api/classification"
+    overall_average = 0.0
+    with open(csv_file) as f:
+        content = csv.reader(f, delimiter=',')
+        next(content, None) #Skip header
+        for line in content:
+            class_name = line[0].strip()
+            if class_name == "Average":
+                overall_average = line[1]
+            elif class_name not in classes and class_name != "NULL":
+                classes = classes + class_name + ","
+    
+    query["className"] = classes[:-1]
+    print("Getting classification map for classes " + query["className"])
+    response = requests.get(function_url, params=query)
+    classification_name_to_class_id = response.json()
+    return overall_average, classification_name_to_class_id
 
 def get_image_name_from_url(image_url):
     start_idx = image_url.rfind('/')+1
@@ -182,7 +234,20 @@ def create_pascal_label_map(label_map_path: str, class_names: list):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config-file', required=True)
+    parser.add_argument(
+        'operation',
+        choices=['start', 'save']
+    )
     args = parser.parse_args()
+    operation = args.operation
     legacy_config = get_legacy_config(args.config_file)
     config = Config.read_config(CONFIG_PATH)
-    train(legacy_config, config.get("tagging_user"), config.get("url"))
+
+    if operation == "start":
+        train(legacy_config, config.get("tagging_user"), config.get("url"))
+    elif operation == "save":
+        # Upload the model saved at ${inference_output_dir}/frozen_inference_graph.pb
+        save_training_session(config, 
+                                legacy_config.get("inference_output_dir") + "/frozen_inference_graph.pb",
+                                legacy_config.get("validation_output"),
+                                legacy_config.get("untagged_output"))
